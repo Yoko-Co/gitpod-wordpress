434b14dfb27550190ad84ff1ce8d5e9ae4feffd3
Konstantin Brazhnik <konst@ntin.solutions>
2021-10-06 22:19:37 -0400

    Dockerfile Image Fix

--- .init.sh
+++ .init.sh
@@ -4,6 +4,19 @@ wp rewrite structure '/%year%/%monthnum%/%postname%/'
 
 wp plugin delete hello
 wp plugin delete akismet
-wp plugin activate ${REPO_NAME}
+wp theme delete twentynineteen
+wp theme delete twentytwenty
+wp theme delete twentytwentyone
+# wp plugin activate ${REPO_NAME}
 
-# wp plugin install plugin_slug --activate
+wp theme activate ${REPO_NAME}
+
+# TODO: Download Live MySQL Dump
+
+# TODO: Import into local MySQL DB
+
+# TODO: Delete MySQL Dump File
+
+# TODO: Download Live Plugins Folder Content
+
+# TODO: Download Live Uploads Content
